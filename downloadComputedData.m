b3cb9d86cbbe0f13d57a595697dbe01a3a21d8fd
Ben Fulcher <ben.d.fulcher@gmail.com>
2016-11-24 16:06:45 +1100

    Update to download script

--- downloadComputedData.m
+++ downloadComputedData.m
@@ -5,7 +5,7 @@ function downloadComputedData()
 url = 'https://ndownloader.figshare.com/files/6294366';
 fileName = 'HCTSA.mat';
 outFileName = websave(fileName,url);
-fprintf(1,'hctsa results for Drosophila melanogaster movement phenotyping downloaded to:\n%s\n',...
-            outFileName);
+fprintf(1,['hctsa results for Drosophila melanogaster movement phenotyping ',...
+            'downloaded from figshare to:\n%s\n'],outFileName);
 
 end
