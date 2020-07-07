aa478af176137e76eee4d670311e04fb232d8116
Ben Fulcher <ben.d.fulcher@gmail.com>
2020-07-07 11:26:13 +1000

    Latest hctsa compatability

--- maleFemaleAnalysis.m
+++ maleFemaleAnalysis.m
@@ -7,12 +7,9 @@
 %-------------------------------------------------------------------------------
 %% Label groups, normalize, and load data:
 % Set how to normalize the data:
-whatNormalization = 'scaledRobustSigmoid'; % 'zscore', 'scaledRobustSigmoid'
+whatNormalization = 'mixedSigmoid'; % 'zscore', 'scaledRobustSigmoid'
 TS_LabelGroups('raw',{'F','M'});
-dataLoad = load('HCTSA.mat');
 TS_Normalize(whatNormalization,[0.5,1]);
-% Load normalized data in a structure:
-normalizedData = load('HCTSA_N.mat');
 
 %-------------------------------------------------------------------------------
 % Reorder features to sit close:
@@ -22,35 +19,44 @@ TS_Cluster('none',[],'corr','average');
 % Reorder to ensure male/female sit together:
 load('HCTSA_N.mat','ts_clust','TimeSeries','TS_DataMat');
 [~,ix] = sort(TimeSeries.Group);
+classLabels = categories(TimeSeries.Group);
 dataMatReOrd = TS_DataMat(ix,:);
-ordering_1 = BF_ClusterReorder([],pdist(dataMatReOrd(TimeSeries.Group(ix)==1,:)),'average');
-ordering_2 = BF_ClusterReorder([],pdist(dataMatReOrd(TimeSeries.Group(ix)==2,:)),'average');
-is1 = ix(TimeSeries.Group(ix)==1);
-ix(TimeSeries.Group(ix)==1) = is1(ordering_1);
-is2 = ix(TimeSeries.Group(ix)==2);
-ix(TimeSeries.Group(ix)==2) = is2(ordering_2);
+ordering_1 = BF_ClusterReorder([],pdist(dataMatReOrd(TimeSeries.Group(ix)==classLabels{1},:)),'average');
+ordering_2 = BF_ClusterReorder([],pdist(dataMatReOrd(TimeSeries.Group(ix)==classLabels{2},:)),'average');
+is1 = ix(TimeSeries.Group(ix)==classLabels{1});
+ix(TimeSeries.Group(ix)==classLabels{1}) = is1(ordering_1);
+is2 = ix(TimeSeries.Group(ix)==classLabels{2});
+ix(TimeSeries.Group(ix)==classLabels{2}) = is2(ordering_2);
 ts_clust.ord = ix;
 save('HCTSA_N.mat','ts_clust','-append')
-dataLoadNorm = load('HCTSA_N.mat');
+fprintf(1,'Saved custom time-series reordering to HCTSA_N.mat\n');
+
+unnormalizedData = load('HCTSA.mat');
+normalizedData = load('HCTSA_N.mat');
+
+%-------------------------------------------------------------------------------
+% Set classification parameters:
+cfnParams = GiveMeDefaultClassificationParams(unnormalizedData);
+cfnParams.whatClassifier = 'svm_linear';
 
 %-------------------------------------------------------------------------------
 % Plot the clustered data matrix:
-TS_PlotDataMatrix('cl','colorGroups',true,'addTimeSeries',false)
+TS_PlotDataMatrix(normalizedData,'colorGroups',true,'addTimeSeries',false)
 
 %-------------------------------------------------------------------------------
 % Get overall classification rate:
-whatClassifier = 'svm_linear';
-TS_Classify(dataLoadNorm,whatClassifier,'numPCs',0);
+numNulls = 0;
+TS_Classify(normalizedData,cfnParams,numNulls);
 
 %-------------------------------------------------------------------------------
 % Male/female PCA plot:
 whatLowDimAlgorithm = 'tSNE'; % 'pca'
 annotateParams = struct('n',12,'textAnnotation','none','userInput',false,'maxL',600);
-TS_PlotLowDim(dataLoadNorm,whatLowDimAlgorithm,true,'',annotateParams);
+TS_PlotLowDim(normalizedData,whatLowDimAlgorithm,true,annotateParams,cfnParams);
 
 %-------------------------------------------------------------------------------
 % Find discriminative features:
-TS_TopFeatures(dataLoad,'fast_linear','numNulls',0,'numFeaturesDistr',40)
+TS_TopFeatures(unnormalizedData,'ustat',struct(),'numNulls',0,'numFeaturesDistr',40)
 
 %-------------------------------------------------------------------------------
 % Plot some of the top features:
@@ -70,14 +76,13 @@ featIDs = [4522,... % SP_Summaries_fft_logdev_q25
 f = figure('color','w');
 for i = 1:length(featIDs)
     subplot(ceil(length(featIDs)/3),3,i);
-    TS_SingleFeature(dataLoad,featIDs(i),true,false);
+    TS_SingleFeature(unnormalizedData,featIDs(i),true,false);
 end
 
-%-------------------------------------------------------------------------------
-% Check out he distributional measure:t
+% Check out he distributional measure:
 annotateParams.maxL = 4320;
-TS_FeatureSummary(1720,dataLoad,true,annotateParams); % remove points -- distribution
-TS_FeatureSummary(4310,dataLoad,true,annotateParams); % spectral flatness measure
+TS_FeatureSummary(1720,unnormalizedData,true,annotateParams); % remove points -- distribution
+TS_FeatureSummary(4310,unnormalizedData,true,annotateParams); % spectral flatness measure
 
 %-------------------------------------------------------------------------------
 % Investigate spectral properties further:
